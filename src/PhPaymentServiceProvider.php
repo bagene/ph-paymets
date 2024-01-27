b8c50c5d71c5864f0db5490de1c47cb6518c3033
Gene Navera <gnavera@gmail.com>
2024-01-27 22:07:29 +0800

    Add Service Provider

---
+++ src/PhPaymentServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Bagene\PhPayments;
+
+use Illuminate\Support\ServiceProvider;
+
+class PhPaymentServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/payments.php' => config_path('payments.php'),
+        ], 'config');
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/payments.php', 'payments');
+    }
+}
